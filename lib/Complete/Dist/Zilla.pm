7ae0ac1f64067ab55d570824aa82323175ce4fb5
perlancar <perlancar@gmail.com>
2014-12-28 13:53:25 +0000

    Import of PERLANCAR/Complete-Dist-Zilla-0.02.tar.gz from CPAN.
    gitpan-cpan-distribution: Complete-Dist-Zilla
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         PERLANCAR/Complete-Dist-Zilla-0.02.tar.gz
    gitpan-cpan-author:       PERLANCAR
    gitpan-cpan-maturity:     released

--- lib/Complete/Dist/Zilla.pm
+++ lib/Complete/Dist/Zilla.pm
@@ -1,7 +1,7 @@
 package Complete::Dist::Zilla;
 
 our $DATE = '2014-12-28'; # DATE
-our $VERSION = '0.01'; # VERSION
+our $VERSION = '0.02'; # VERSION
 
 use 5.010001;
 use strict;
@@ -19,10 +19,7 @@ our @EXPORT_OK = qw(
                        complete_dzil_role
                );
 
-$SPEC{complete_dzil_plugin} = {
-    v => 1.1,
-    summary => 'Complete with installed Dist::Zilla plugin names',
-    args => {
+my %common_args = (
         word => {
             schema => 'str*',
             req => 1,
@@ -38,6 +35,13 @@ $SPEC{complete_dzil_plugin} = {
         exp_im_path => {
             schema => 'bool',
         },
+);
+
+$SPEC{complete_dzil_plugin} = {
+    v => 1.1,
+    summary => 'Complete with installed Dist::Zilla plugin names',
+    args => {
+        %common_args,
     },
     result_naked => 1,
 };
@@ -58,6 +62,56 @@ sub complete_dzil_plugin {
     );
 }
 
+$SPEC{complete_dzil_role} = {
+    v => 1.1,
+    summary => 'Complete with installed Dist::Zilla role names',
+    args => {
+        %common_args,
+    },
+    result_naked => 1,
+};
+sub complete_dzil_role {
+    require Complete::Module;
+
+    my %args = @_;
+
+    my $word = $args{word} // '';
+    my $ci          = $args{ci} // $Complete::OPT_CI;
+    my $map_case    = $args{map_case} // $Complete::OPT_MAP_CASE;
+    my $exp_im_path = $args{exp_im_path} // $Complete::OPT_EXP_IM_PATH;
+
+    Complete::Module::complete_module(
+        word => $word,
+        ns_prefix => 'Dist::Zilla::Role',
+        ci=>$ci, map_case=>$map_case, exp_im_path=>$exp_im_path,
+    );
+}
+
+$SPEC{complete_dzil_bundle} = {
+    v => 1.1,
+    summary => 'Complete with installed Dist::Zilla bundle names',
+    args => {
+        %common_args,
+    },
+    result_naked => 1,
+};
+sub complete_dzil_bundle {
+    require Complete::Module;
+
+    my %args = @_;
+
+    my $word = $args{word} // '';
+    my $ci          = $args{ci} // $Complete::OPT_CI;
+    my $map_case    = $args{map_case} // $Complete::OPT_MAP_CASE;
+    my $exp_im_path = $args{exp_im_path} // $Complete::OPT_EXP_IM_PATH;
+
+    Complete::Module::complete_module(
+        word => $word,
+        ns_prefix => 'Dist::Zilla::PluginBundle',
+        ci=>$ci, map_case=>$map_case, exp_im_path=>$exp_im_path,
+    );
+}
+
 1;
 # ABSTRACT: Complete with installed Dist::Zilla plugin names
 
@@ -73,16 +127,66 @@ Complete::Dist::Zilla - Complete with installed Dist::Zilla plugin names
 
 =head1 VERSION
 
-This document describes version 0.01 of Complete::Dist::Zilla (from Perl distribution Complete-Dist-Zilla), released on 2014-12-28.
+This document describes version 0.02 of Complete::Dist::Zilla (from Perl distribution Complete-Dist-Zilla), released on 2014-12-28.
 
 =head1 SYNOPSIS
 
 =head1 FUNCTIONS
 
 
+=head2 complete_dzil_bundle(%args) -> any
+
+Complete with installed Dist::Zilla bundle names.
+
+Arguments ('*' denotes required arguments):
+
+=over 4
+
+=item * B<ci> => I<bool>
+
+Whether to do case-insensitive search.
+
+=item * B<exp_im_path> => I<bool>
+
+=item * B<map_case> => I<bool>
+
+=item * B<word>* => I<str>
+
+=back
+
+Return value:
+
+ (any)
+
+
 =head2 complete_dzil_plugin(%args) -> any
 
 Complete with installed Dist::Zilla plugin names.
+
+Arguments ('*' denotes required arguments):
+
+=over 4
+
+=item * B<ci> => I<bool>
+
+Whether to do case-insensitive search.
+
+=item * B<exp_im_path> => I<bool>
+
+=item * B<map_case> => I<bool>
+
+=item * B<word>* => I<str>
+
+=back
+
+Return value:
+
+ (any)
+
+
+=head2 complete_dzil_role(%args) -> any
+
+Complete with installed Dist::Zilla role names.
 
 Arguments ('*' denotes required arguments):
 
